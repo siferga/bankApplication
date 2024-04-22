c7b2c16e168d862cec44bea3efc8a4ed484832a1
Siferga <sifergadeveloper@gmail.com>
2024-04-22 09:20:11 +0200

    first comment

---
+++ ms-Accounts/src/main/java/com/siferga/msAccounts/MsAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.siferga.msAccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAccountsApplication.class, args);
+	}
+
+}
