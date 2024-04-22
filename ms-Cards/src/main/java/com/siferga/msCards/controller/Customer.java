51c7d39ef7656754161b7a307f312769eb4b7fc8
Siferga <sifergadeveloper@gmail.com>
2024-04-22 15:18:22 +0200

    classes

---
+++ ms-Cards/src/main/java/com/siferga/msCards/controller/Customer.java
@@ -0,0 +1,10 @@
+package com.siferga.msCards.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class Customer {
+    private int id;
+}
