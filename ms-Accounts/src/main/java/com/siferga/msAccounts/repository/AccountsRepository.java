71ef135485dee5109337aee3919b88f2cc7c61c1
Siferga <sifergadeveloper@gmail.com>
2024-05-10 10:08:26 +0200

    repo account

--- ms-Accounts/src/main/java/com/siferga/msAccounts/repository/AccountsRepository.java
+++ ms-Accounts/src/main/java/com/siferga/msAccounts/repository/AccountsRepository.java
@@ -1,11 +1,8 @@
 package com.siferga.msAccounts.repository;
 
 import com.siferga.msAccounts.model.Accounts;
-import com.siferga.msAccounts.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Integer> {
