5d07c4f7d920c2aae2bef474f5d271ad6297f168
Siferga <sifergadeveloper@gmail.com>
2024-05-11 06:01:55 +0200

    data.sql cards

--- ms-Cards/src/main/resources/data.sql
+++ ms-Cards/src/main/resources/data.sql
@@ -5,21 +5,21 @@ DROP TABLE IF EXISTS cards;
 CREATE TABLE `cards` (
     `card_id` INT NOT NULL AUTO_INCREMENT,
     `card_number` VARCHAR(100) NOT NULL,
-    `customer_id` int NOT NULL,
+    `customer_id` INT NOT NULL,
     `card_type` VARCHAR(100) NOT NULL,
     `total_limit` INT NOT NULL,
     `amount_used` int NOT NULL,
-    `available_amount` VARCHAR(100) NOT NULL,
+    `available_amount` int NOT NULL,
     `create_dt` date  DEFAULT NULL,
-    PRIMARY KEY ('card_id')
+    PRIMARY KEY (`card_id`)
 );
 
 
 INSERT INTO `cards` (`card_number`,`customer_id`,`card_type`,`total_limit`,`amount_used`,`available_amount` ,`create_dt`)
-VALUES ('4545XXXXXX45445','1','Credit', 10000, 500 , 9500, CURDATE());
+VALUES ('4545XXXXXX45',1,'Credit', 10000, 500 , 9500, CURDATE());
 
 INSERT INTO `cards` (`card_number`,`customer_id`,`card_type`,`total_limit`,`amount_used`,`available_amount` ,`create_dt`)
-VALUES ('4545XXXXXX45445','1','Credit', 7500, 600, 6900, CURDATE());
+VALUES ('4545XXXXXX44',1,'Credit', 7500, 600, 6900, CURDATE());
 
 INSERT INTO `cards` (`card_number`,`customer_id`,`card_type`,`total_limit`,`amount_used`,`available_amount` ,`create_dt`)
-VALUES ('4545XXXXXX45445','1','Credit', 20000, 4000, 16000, CURDATE());
+VALUES ('4545XXXXXX46',1,'Credit', 20000, 4000, 16000, CURDATE());
