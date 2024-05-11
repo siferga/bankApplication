f792a241916fe61976417a54fbeea6dd56ff675d
Siferga <sifergadeveloper@gmail.com>
2024-05-11 22:02:05 +0200

    loans customer model

---
+++ ms-Loans/src/main/java/com/siferga/msLoans/model/Customer.java
@@ -0,0 +1,12 @@
+package com.siferga.msLoans.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter @Setter @ToString
+public class Customer {
+    private int customerId;
+}
