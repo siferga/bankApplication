21f1a2d01045e54005096685e9284c1dcbec8974
Siferga <sifergadeveloper@gmail.com>
2024-05-11 22:00:52 +0200

    cards controller et repo update

--- ms-Cards/src/main/java/com/siferga/msCards/controller/CardsController.java
+++ ms-Cards/src/main/java/com/siferga/msCards/controller/CardsController.java
@@ -3,12 +3,16 @@ package com.siferga.msCards.controller;
 import com.siferga.msCards.model.Cards;
 import com.siferga.msCards.model.Customer;
 import com.siferga.msCards.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CardsController {
+
     private final CardsRepository cardsRepository;
 
     public CardsController(CardsRepository cardsRepository) {
@@ -16,8 +20,10 @@ public class CardsController {
     }
 
     @PostMapping("/myCards")
-    public Cards getCardsDetails(@RequestBody Customer customer) {
-        return cardsRepository.findByCustomerId(customer.getCustomerId());
+    public List<Cards> getCardsDetails(@RequestBody Customer customer) {
+         List<Cards> Cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        return Cards;
     }
+
 
 }
--- ms-Cards/src/main/java/com/siferga/msCards/repository/CardsRepository.java
+++ ms-Cards/src/main/java/com/siferga/msCards/repository/CardsRepository.java
@@ -4,7 +4,9 @@ import com.siferga.msCards.model.Cards;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardsRepository extends JpaRepository<Cards, Integer> {
-    Cards findByCustomerId(int customerId);
+    List<Cards> findByCustomerId(int customerId);
 }
