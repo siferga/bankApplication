895eaddbb373f1de527ec5caa5bb5f594be25cbb
Siferga <sifergadeveloper@gmail.com>
2024-05-16 10:07:06 +0200

    loans ms update

--- ms-Loans/src/main/java/com/siferga/msLoans/MsLoansApplication.java
+++ ms-Loans/src/main/java/com/siferga/msLoans/MsLoansApplication.java
@@ -2,8 +2,19 @@ package com.siferga.msLoans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@RefreshScope
+@ComponentScans({ @ComponentScan("com.siferga.msLoans.controller")})
+@EnableJpaRepositories("com.siferga.msLoans.repository")
+@EntityScan("com.siferga.msLoans.model")
 public class MsLoansApplication {
 
 	public static void main(String[] args) {
