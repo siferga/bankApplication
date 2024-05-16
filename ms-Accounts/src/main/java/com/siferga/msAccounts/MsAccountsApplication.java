cc62243d3c51b9b234137b9d1e7225dd446acd49
Siferga <sifergadeveloper@gmail.com>
2024-05-16 10:05:57 +0200

    ms account update

--- ms-Accounts/src/main/java/com/siferga/msAccounts/MsAccountsApplication.java
+++ ms-Accounts/src/main/java/com/siferga/msAccounts/MsAccountsApplication.java
@@ -2,8 +2,19 @@ package com.siferga.msAccounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@RefreshScope
+@ComponentScans({ @ComponentScan("com.siferga.msAccounts.controller")})
+@EnableJpaRepositories("com.siferga.msAccounts.repository")
+@EntityScan("com.siferga.msAccounts.model")
 public class MsAccountsApplication {
 
 	public static void main(String[] args) {
