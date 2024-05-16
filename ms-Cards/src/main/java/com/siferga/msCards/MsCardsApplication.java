59dd9e87ceda905ac5b7acaeaf5cfaf702af04f7
Siferga <sifergadeveloper@gmail.com>
2024-05-16 10:06:52 +0200

    cards ms update

--- ms-Cards/src/main/java/com/siferga/msCards/MsCardsApplication.java
+++ ms-Cards/src/main/java/com/siferga/msCards/MsCardsApplication.java
@@ -2,8 +2,19 @@ package com.siferga.msCards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@RefreshScope
+@ComponentScans({ @ComponentScan("com.siferga.msCards.controller")})
+@EnableJpaRepositories("com.siferga.msCards.repository")
+@EntityScan("com.siferga.msCards.model")
 public class MsCardsApplication {
 
 	public static void main(String[] args) {
